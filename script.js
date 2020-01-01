28cda1f0e574946b353b5ea0fcf5ac1fd032d1bb
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-01-01 15:22:18 -0600

    Wrote a variable for my prompt to request the number of characters the user wants for the password

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~"];
+var numOfCharacters = prompt("How many characters do you want your password to contain?");
 
 // Write password to the #password input
 function writePassword() {
