64d20ae683db603e2a70aa80dc791b47ce90f52e
Weston Rose <westonrose@Westons-MacBook-Air.local>
2020-01-04 14:13:16 -0600

    With the help of my tutor, I discovered that my function called generatePassword was not working because I did not properly close my curly brackets. I created a function for each confirm statement to ask if the user wanted to include a certain array, for example uppercase letters. These functions were called within the generatePassword function.Also, the tutor guided me to create an if statement in the generatrePassword function to account for the possibility that a user does NOT choose to have any array, in which case an alert message would tell the user to choose at least one type of character and the user would cycle through the options again. Finally, I created a for loop that randomized chosen arrays into an array called availableCharacter. Those chosen arrays were also randomized using Math.floor and Math.random.

--- script.js
+++ script.js
@@ -3,39 +3,123 @@ var generateBtn = document.querySelector("#generate");
 var passwordLength = 0;
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", '9'];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~"];
+var availableCharacter = [];
+
+// var passwordPrompt = prompt("How many characters do you want your password to contain?");
+//     if(passwordPrompt > 7 && passwordPrompt < 128) {
+//         passwordLength = passwordPrompt;
+//         isValidationPass = true;
+//     } 
+//     else {
+//         alert("Password must be more than 8 characters and less than 128 characters")
+//     }
+// var password = generatePassword();
+// var passwordText = document.querySelector("#password");
+// passwordText.value = password;
+
 function generatePassword() {
     // Validate length of password. Use a while loop to repeat validation unil user types in correct input
+    var newPassword = "";
+    availableCharacter = [];
     var isValidationPass = false;
-    while(isValidationPass == false) {
-        var passwordPrompt = prompt("How many characters do you want your password to contain?");
-        if(passwordPrompt > 7 && passwordPrompt < 128) {
+    while (isValidationPass == false) {
+        var passwordPrompt = parseInt(prompt("How many characters do you want your password to contain?"));
+        console.log(typeof passwordPrompt);
+        if (passwordPrompt > 7 && passwordPrompt < 129) {
             passwordLength = passwordPrompt; // using parseInt to convert string to integer
-            alert("Yay password validation worked. Length is set to " + passwordLength); // Weston feel free to remove this alert
+            // alert("Yay password validation worked. Length is set to " + passwordLength); // Weston feel free to remove this alert
             isValidationPass = true; // this will force while loop to end
-        }
-        else {
+        } else {
             alert("Password must be more than 8 characters and less than 128 characters.")
         }
-    ///////// Write code below for password generation ///////////
+    }
+    wantUppercase()
+    wantLowercase()
+    wantNumbers()
+    wantSpecialCharacters()
+    if (availableCharacter.length === 0) {
+        alert("You must choose at least one type of character!")
+        generatePassword()
+    } //may needs an else statement here
+    for (alpha = 0; alpha < passwordPrompt; alpha++) {
+        var characterGroupSelector = availableCharacter[Math.floor(Math.random() * availableCharacter.length)];
+        var characterSelector = characterGroupSelector[Math.floor(Math.random() * characterGroupSelector.length)];
+        newPassword = newPassword.concat(characterSelector);
+    }
+    /////// Write code below for password generation ///////////
     // Weston once you have your logic to randomize and generate a password you'll have to update the
     // variable 'genratedPassword' with your random password
     // Right now this is just a placeholder and will always output 'MY_GENERATED_PASSWORD_HERE' as the random password
-    var genratedPassword = "MY_GENERATED_PASSWORD_HERE";
+    // var genratedPassword = "MY_GENERATED_PASSWORD_HERE";
     // Insert Code Here
-    return genratedPassword; // return results of generated password. Shouldn't have to modify this line.
+    return newPassword; // return results of generated password. Shouldn't have to modify this line.
 }
+
+function wantUppercase() {
+    var isUppercase = confirm("Do you want uppercase letters?");
+    if (isUppercase) {
+        availableCharacter = availableCharacter.concat(uppercaseLetters);
+        console.log(availableCharacter);
+    }
+
+}
+
+
+function wantLowercase() {
+    var isLowercase = confirm("Do you want lowercase letters?");
+    if (isLowercase) {
+        availableCharacter = availableCharacter.concat(lowercaseLetters);
+        console.log(availableCharacter);
+    }
+
+}
+
+
+function wantNumbers() {
+    var isNumbers = confirm("Do you want numbers?");
+    if (isNumbers) {
+        availableCharacter = availableCharacter.concat(numbers);
+        console.log(availableCharacter);
+    }
+
+}
+
+
+function wantSpecialCharacters() {
+    var isSpecialCharacters = confirm("Do you want special characters?");
+    if (isSpecialCharacters) {
+        availableCharacter = availableCharacter.concat(specialCharacters);
+        console.log(availableCharacter);
+    }
+
+}
+
 // Write password to the #password input
+// function writePassword() {
+//   var clickPassword = prompt("How many characters do you want your password to contain?");
+//     if(clickPassword <= 7) {
+//         alert("Must be at least 8 characters.");
+//     } else {
+//         (clickPassword >= 129) 
+//         alert("Must be less than 128 characters.");
+//     }
+// }
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+    // copyBtn.removeAttribute("disabled");
+    // copyBtn.focus();
 }
+
+
+
 function copyToClipboard() {
-  // BONUS 
+    // BONUS 
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
